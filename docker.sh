7a8f4ef81ec29706ccb93247f9ad647784ed8191
saipranayteja <pranayteja.ch@algonox.com>
2023-07-26 13:04:12 +0530

    sdxcf

---
+++ docker.sh
@@ -0,0 +1,10 @@
+yum remove docker \
+           docker-client \
+           docker-client-latest \
+           docker-common \
+           docker-latest \
+           docker-latest-logrotate \
+           docker-logrotate \
+           docker-engine \
+           podman \
+           runc
