4655307e305ba128e1176a254fd432a3233838b3
saipranayteja <pranayteja.ch@algonox.com>
2023-07-26 14:11:13 +0530

    xdf

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ docker_setup () {
 
     yum install -y yum-utils device-mapper-persistent-data lvm2
 
-    yum-config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+    yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 
     yum install docker
 
