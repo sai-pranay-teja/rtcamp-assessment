42ccc0cc573284fe235b73fb40465ce756732b97
saipranayteja <pranayteja.ch@algonox.com>
2023-07-27 23:55:40 +0530

    sdfc

--- manage.sh
+++ manage.sh
@@ -2,8 +2,10 @@ if [ $1 = "start"]; then
     docker run --name mariadb --env MARIADB_USER=admin --env MARIADB_PASSWORD=123 --env MARIADB_ROOT_PASSWORD=123 pranayroot/mariadb:v1
     docker run -d --name php pranayroot/php:v1
     docker run -d --name nginx pranayroot/nginx:v1
+fi
 
 
 
 if [ $1 = "load" ]; then
     bash mariadb/files/run.sh
+fi
