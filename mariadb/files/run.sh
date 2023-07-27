9615a1fb1aec16bde95233061f8b21c57ca5a5cc
saipranayteja <pranayteja.ch@algonox.com>
2023-07-27 19:16:52 +0530

    dcf

--- mariadb/files/run.sh
+++ mariadb/files/run.sh
@@ -1,3 +1,3 @@
-echo 'grant all privileges on *.* to 'admin'@'%';'| mariadb -uroot -p123
-echo 'FLUSH PRIVILEGES;'; | mariadb -uroot -p123
-echo 'CREATE DATABASE db_name1;' | mariadb -uadmin -p123
+echo "grant all privileges on *.* to 'admin'@'%';"| mariadb -uroot -p123
+echo "FLUSH PRIVILEGES;" | mariadb -uroot -p123
+echo "CREATE DATABASE db_name1;" | mariadb -uadmin -p123
