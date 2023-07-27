507d7bbedc680aec0cf64927934d8812f077d222
saipranayteja <pranayteja.ch@algonox.com>
2023-07-27 19:07:52 +0530

    sxd

--- mariadb/files/run.sh
+++ mariadb/files/run.sh
@@ -1,4 +1,4 @@
-echo 'CREATE USER 'admin'@'localhost' IDENTIFIED BY '123';'
-echo 'grant all privileges on *.* to 'admin'@'localhost';'
-echo 'FLUSH PRIVILEGES';
+echo 'CREATE USER 'admin'@'%' IDENTIFIED BY '123';' | mariadb -uroot -p123
+echo 'grant all privileges on *.* to 'admin'@'%';'| mariadb -uroot -p123
+echo 'FLUSH PRIVILEGES;'; | mariadb -uroot -p123
 echo 'CREATE DATABASE db_name1;' | mariadb -uadmin -p123
