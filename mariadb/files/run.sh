19ee9aac7bb778b78946abc5d89ffd3bacf44dc5
saipranayteja <pranayteja.ch@algonox.com>
2023-07-27 21:38:50 +0530

    sdf

--- mariadb/files/run.sh
+++ mariadb/files/run.sh
@@ -1,11 +1,11 @@
-until mysqladmin -uroot -p123  ping; do
-   echo "$(date) - waiting for mysql"
-   sleep 3
-done
+# until mysqladmin -uroot -p123  ping; do
+#    echo "$(date) - waiting for mysql"
+#    sleep 3
+# done
 
-if ! mariadb -uroot -p123 -e -h db -e 'use dbname'; then
-   mariadb -uroot -p123 -e "CREATE DATABASE dbname"
-fi
+# if ! mariadb -uroot -p123 -e -h db -e 'use dbname'; then
+#    mariadb -uroot -p123 -e "CREATE DATABASE dbname"
+# fi
 
-exec "$@"
+# exec "$@"
 
