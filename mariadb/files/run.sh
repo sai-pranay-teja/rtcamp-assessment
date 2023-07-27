ab903e20d553d2671371fc46aeff8341fa40705e
saipranayteja <pranayteja.ch@algonox.com>
2023-07-27 19:14:41 +0530

    sdf

--- mariadb/files/run.sh
+++ mariadb/files/run.sh
@@ -1,4 +1,3 @@
-echo 'CREATE USER 'admin'@'%' IDENTIFIED BY '123';' | mariadb -uroot -p123
 echo 'grant all privileges on *.* to 'admin'@'%';'| mariadb -uroot -p123
 echo 'FLUSH PRIVILEGES;'; | mariadb -uroot -p123
 echo 'CREATE DATABASE db_name1;' | mariadb -uadmin -p123
