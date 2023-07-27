c49ff266b555a985632fddf2849fb54b0015ca26
saipranayteja <pranayteja.ch@algonox.com>
2023-07-27 19:36:24 +0530

    sdcf

--- mariadb/files/run.sh
+++ mariadb/files/run.sh
@@ -1,4 +1,4 @@
-echo "grant all privileges on *.* to 'admin'@'%';"| mariadb -uroot -p123
+echo "grant all privileges on *.* to 'admin'@'%' IDENTIFIED BY '123';"| mariadb -uroot -p123
 
 
 echo "FLUSH PRIVILEGES;" | mariadb -uroot -p123
@@ -10,12 +10,10 @@ echo "CREATE DATABASE db_name1;" | mariadb -uadmin -p123
 echo "CREATE DATABASE wordpress_db;" | mariadb -uadmin -p123
 
 
-echo "CREATE DATABASE wordpress_db;" | mariadb -uadmin -p123
 
+echo "GRANT ALL ON wordpress_db.* TO 'admin'@'%' IDENTIFIED BY '123';" | mariadb -uroot -p123
 
-echo "GRANT ALL ON wordpress_db.* TO 'admin'@'%' IDENTIFIED BY '123';" | mariadb -uadmin -p123
-
-echo "FLUSH PRIVILEGES;" | mariadb -uroot -p123
+echo "FLUSH PRIVILEGES;" | mariadb -uadmin -p123
 
 
 
