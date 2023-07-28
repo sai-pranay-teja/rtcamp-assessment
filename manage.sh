7fc7f5eef08bf280431729b632a58ea2426b64e3
saipranayteja <pranayteja.ch@algonox.com>
2023-07-28 21:56:45 +0530

    iuy

--- manage.sh
+++ manage.sh
@@ -1,12 +1,18 @@
-if [ $1 = "start" ]; then
-    docker run --name mariadb --env MARIADB_USER=admin --env MARIADB_PASSWORD=123 --env MARIADB_ROOT_PASSWORD=123 pranayroot/mariadb:v1
-    docker run -d --name php pranayroot/php:v1
-    docker run -d --name nginx pranayroot/nginx:v1
-fi
+# if [ $1 = "start" ]; then
+#     docker run --name mariadb --env MARIADB_USER=admin --env MARIADB_PASSWORD=123 --env MARIADB_ROOT_PASSWORD=123 pranayroot/mariadb:v1
+#     docker run -d --name php pranayroot/php:v1
+#     docker run -d --name nginx pranayroot/nginx:v1
+# fi
 
 
 
-if [ $1 = "load" ]; then
-    docker exec -it mariadb bash run.sh
-    docker restart mariadb
-fi
+# if [ $1 = "load" ]; then
+#     docker exec -it mariadb bash run.sh
+#     docker restart mariadb
+# fi
+
+git clone https://github.com/WordPress/WordPress
+
+cd WordPress
+
+docker build -t pranayroot/lemp:v1 .
