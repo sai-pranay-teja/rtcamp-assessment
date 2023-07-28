a6a3e795c213901c95c9174f11ad45e13fbffa68
saipranayteja <pranayteja.ch@algonox.com>
2023-07-29 01:00:04 +0530

    sdfcvg

--- manage.sh
+++ manage.sh
@@ -1,3 +1,8 @@
+DIR="/home/ec2-user/rtcamp-assessment/wordpress-docker"
+if [ ! -d "$DIR" ]; then
+   mkdir  /home/ec2-user/rtcamp-assessment/wordpress-docker
+fi
+
 mkdir wordpress-docker
 cd wordpress-docker
 mkdir public nginx
